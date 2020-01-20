6c83aa92b39c68362853cb35e3fbfffa212cd355
Yu Chunjian <yuchunjian@Yus-MacBook-Pro.local>
2020-01-20 17:24:21 +0800

    ent

--- server/app.js
+++ server/app.js
@@ -79,7 +79,7 @@ const routerAuction = require('./router-auction');
     auctions1.push({ state: 0, price: 70000, reserve: 100000, carid: 20 });
     auctions1.push({ state: 0, price: 50000, reserve: 80000, carid: 21 });
     const tasks = [];
-    tasks.push({ id: 0, state: 0, auctions: auctions1, start_time: new Date('2020-01-19 13:00') });
+    tasks.push({ id: 0, state: 0, auctions: auctions1, start_time: new Date('2020-01-20 08:46') });
     tasks.push({ id: 1, state: 0, auctions: auctions2, start_time: new Date('2020-01-19 08:50') });
 
     // ev.on('next', () => {
